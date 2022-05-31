29438ef1243dc357dc5c43c2b0a54219a0bd37a1
Igor Sokol <isaexecuter@gmail.com>
2022-05-31 15:13:47 +0300

    Add Vector2 binding in input action

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
 public class PlayerShooter : MonoBehaviour
 {
-    public void OnShoot(InputAction.CallbackContext context)
+    private PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+
+        _playerInput.Player.Shoot.performed += ctx => OnShoot();
+        _playerInput.Player.Move.performed += ctx => OnMove();
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    public void OnShoot()
     {
         Debug.Log("Shoot");
+    }
+
+    public void OnMove()
+    {
+        Vector2 moveDirDirection = _playerInput.Player.Move.ReadValue<Vector2>();
+
+        Debug.Log(moveDirDirection);
     }
 }
